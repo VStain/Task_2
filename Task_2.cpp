640dc417786aa083efc97bbc2d5123b9b9a831cf
Vladislav M <vl.moshkov@gmail.com>
2022-07-01 21:29:30 +0300

    fix_5

--- Task_2.cpp
+++ Task_2.cpp
@@ -9,23 +9,30 @@ struct Client_information
     int balance = 0;
 };
 
+void Change_balance(Client_information* info, int new_balance)
+{
+    cout << "Введите новый баланс :";
+    cin >> new_balance;
+    info->balance = new_balance;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Rus");
 
     Client_information info;
     info.account_number = 0;
-    info.owners_name;
-    info.balance;
+    int new_balance = 0;
 
     cout << "Введите номер счёта :" ;
     cin >> info.account_number;
     cout <<  "Введите имя владельца :";
     cin >> info.owners_name;
-    cout << "Введите баланс :" ;
+    cout << "Введите баланс :";
     cin >> info.balance;
-    cout << "Введите новый баланс :";
-    cin >> info.balance;
+
+    Change_balance(&info, new_balance);
+
     cout << "Ваш счёт :" << info.owners_name << ", " << info.account_number << ", " << info.balance << endl;
 
 
